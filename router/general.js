545ec50dc942a2ad64a32f3c47c2c0a823a7fc0d
swsanket <workwithsanketpadgal@gmail.com>
2024-11-28 15:17:55 +0530

    task5-getreviews

--- router/general.js
+++ router/general.js
@@ -57,8 +57,14 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;  
+  const book = books[isbn];
+  const reviews = book.reviews;
+  if (book) {
+    return res.status(200).json(reviews);
+  } else {
+    return res.status(404).json({ message: "reviews not found" });
+  }
 });
 
 module.exports.general = public_users;
