32b6ef821efec0764630d34fd407f3e6a0a21aff
swsanket <workwithsanketpadgal@gmail.com>
2024-11-28 15:07:28 +0530

    task2&3-getsbookbyisbn&byauthor

--- router/general.js
+++ router/general.js
@@ -20,16 +20,28 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;  
+  const book = books[isbn];  
+  if (book) {
+    return res.status(200).json(book);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const authorName = req.params.author;  
+  const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase().includes(authorName.toLowerCase()));
+  
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: `No books found by author: ${authorName}` });
+  }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
